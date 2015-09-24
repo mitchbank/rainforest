b53cda07a191172eca724c48cd610d44c9984277
Mitchell Bank <mitchbank26@gmail.com>
2015-09-24 15:37:38 -0400

    Search functionality now only uses $.getScript

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -4,11 +4,7 @@ $(document).on('ready page:load', function(){
 		event.preventDefault();
 		var searchValue = $('#search').val();
 
-	$.get('/products?search=' + searchValue)
-		.done(function(data){
-		console.log(data)
-		$('#products').html(data);
-	});
+	$.getScript('/products?search=' + searchValue);
 });
 
 
