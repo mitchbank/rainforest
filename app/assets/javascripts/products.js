d45141ef4486990f76e79f72b8ca0e1aa686881e
Mitchell Bank <mitchbank26@gmail.com>
2015-09-24 17:11:20 -0400

    cleaned up infinite scroll

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -7,22 +7,11 @@ $(document).on('ready page:load', function(){
 	$.getScript('/products?search=' + searchValue);
 	});
 
-
-	$(window).scroll(function() {
-		if ($(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-			console.log($('.pagination span.next').children().attr('href'));
-			$.getScript($('.pagination span.next').children().attr('href'));
+$(window).scroll(function() {
+		var url = $('.pagination span.next').children().attr('href');
+		if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+			$('.pagination').text("Fetching more products...");
+			return $.getScript(url);
 		}
 	});
 });
-
-
-
-
-// $(window).scroll(function() {
-// 		var url = $('.pagination span.next').children().attr('href');
-// 		if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-			
-// 		}
-// 	});
-// });
